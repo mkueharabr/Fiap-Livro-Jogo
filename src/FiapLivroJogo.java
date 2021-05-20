80dd23b64f6d91edfc6f748f7a9c3b09991e943e
mkueharabr <marcelo.uehara@gmail.com>
2021-05-20 20:00:55 -0300

    Implementado cenas 01 e 02

--- src/FiapLivroJogo.java
+++ src/FiapLivroJogo.java
@@ -15,29 +15,53 @@ public class FiapLivroJogo {
 	private static int idadeGamer;
 	private static String nomeGamer;
 	private static final int idadeMinimaGamer = 16;
+	private static boolean continuaSaga = true;
 	
 	private static Scanner leitor = new Scanner(System.in);
 	
+	private static enum StatusSaude {
+		VIVO, MORTO, DOENTE, ZUMBI
+	}
+	
+	private static StatusSaude statusAtual;
 	
 	public static void main(String[] args) throws InterruptedException, IOException {
+		statusAtual = StatusSaude.VIVO;
+		String resposta;
 		
-		boolean resposta = apresentacao();
-
+		continuaSaga = apresentacao();
 		
-		while(resposta) {
-			
-			
+		if (continuaSaga) {
 			limpaTela();
 			abertura();
 			
-			limpaTela();	
-			cenaKlingdonSentarJunto();
+			while(continuaSaga) {
+						
+				limpaTela();	
+				cenaKlingdonSentarJunto();
+				
+				limpaTela();
+				continuaSaga = cenaKlingdonNaMesa();
+				
+				rolarTela(10,0);
+				imprimeTexto(gameOver());
+				
+				System.out.println("Sua saga chegou a um final. Você gostaria de continuar e conhecer outras possibilidades?");
+				System.out.println("Digite (S - Sim, N - Não): ");
+				resposta = leitor.next();		
+				
+				if (resposta.equalsIgnoreCase("N")) {
+					continuaSaga = false;
+				} else {
+					continuaSaga = true;
+				}
+			}
 			
-			resposta = false;
+		} else {
+			rolarTela(10,0);
+			imprimeTexto(gameOver());
+			
 		}
-		
-		
-		gameOver();
 		
 		leitor.close();
 
@@ -66,9 +90,7 @@ public class FiapLivroJogo {
 			System.out.println("Olá " + nomeGamer + ". Infelizmente esse jogo não é recomendado para menores de " + 
 					idadeMinimaGamer + " anos.");
 			continuar = false;
-			rolarTela(5,0);
-			
-			imprimeTexto(gameOver());
+
 			
 		} else {
 			limpaTela();
@@ -86,13 +108,10 @@ public class FiapLivroJogo {
 				limpaTela();
 				System.out.println("Obrigado por sua sábia decisão.");
 				continuar = false;
-				rolarTela(5,0);
-				imprimeTexto(gameOver());
+
 			}
 		}
-		
-		
-		
+
 		return continuar;
 	}
 
@@ -123,7 +142,7 @@ public class FiapLivroJogo {
 	private static void imprimeTexto(String[] texto) {
 		for (String linha : texto) {
 			if (linha == "") {
-				pausaMiliSegundos(5000);
+				pausaMiliSegundos(2000);
 			}
 			System.out.println(linha);
 		}
@@ -209,29 +228,31 @@ public class FiapLivroJogo {
 							"Chega de spoilers... Para falar a verdade, poucos irão ler até aqui. Se você chegou até esse ponto",
 							"meus sinceros agradecimentos ;). Você é um guerreiro. Enfim, esse é o cenário atual que vive nosso herói.",
 							"",
-							nomeGamer + ", essa missão caso aceite-a, você decidirá o que é fato ou fake e o destino dele e ",
-							"de todos os habitantes da galáxia desse reino não tão tão distante!",
+							nomeGamer + ", essa missão caso aceite-a, você decidirá o que é fato ou fake, qual caminho deve ser tomado",
+							"além do destino dele e de todos os habitantes da galáxia e desse reino não tão tão distante!",
 							"",
 							nomeGamer + ", se vc for capturado, morto ou virar zumbi, negaremos tudo perante à nação.",
 							"Boa sorte! Bom jogo e que a força... da decisão esteja com vc!",
 							"",
 							"Alerta: Essa é uma obra de ficção. E qualquer personagem, lugar, acontecimentos citados aqui não",
 							"foram baseados em fatos reais! Esse jogo NÃO faz referências a nenhum filme. É pura ficção... científica!",
+							"Podemos garantir que durante os seus " + idadeGamer + " anos de vida você não viu nada igual.",
+							"",
 							"Para uma melhor experiência gere o .jar e execute-o na linha de comando."
 		};
 		
 	
-		imprimeCaracter(intro, 15);
+		imprimeCaracter(intro, 10);
 		System.out.println("");
 		
 		pausaMiliSegundos(1000);
 		
-		imprimeCaracter(titulo,20);
+		imprimeCaracter(titulo,10);
 		System.out.println("");
 		
 		pausaMiliSegundos(1000);
 		
-		imprimeCaracter(resumo, 20);
+		imprimeCaracter(resumo, 5);
 		
 		pausaMiliSegundos(3000);
 		rolarTela(10, 300);
@@ -250,22 +271,23 @@ public class FiapLivroJogo {
 				"Nosso herói está a bordo do intergalático cruzador MK70xx fazendo reparos no hiper                           ",
 				"computador quântico PulsarVx, quando pela janelinha avista o 'planetinha azul' e imagina                     ",
 				"quando retornará para casa.                                                                                  ",
-				"                                                                                                             ",
-				"Mario está há 3 meses no espaço indo de planeta a´planeta e de nave a nave para fazer a                      ",
+				"",
+				"Mario está há 3 meses no espaço indo de planeta em planeta e de nave a nave para fazer a                      ",
 				"manutenção de uma lista enorme de equipamentos e conta na sua equipe com seres de vários                     ",
 				"locais distantes da galáxia, além de alguns droides.                                                         ",
-				"                                                                                                             ",
+				"",
 				"Na pausa do almoço, dois colegas Klingdons se aproximam da mesa e pergunta se os lugares                     ",
 				"estão vagos. Diz as más linguas que a doença zumbi se originou no planeta deles e foi                        ",
-				"fabricando por algum cientista.                                                                              ",
+				"fabricado por algum cientista.                                                                              ",
+				"",
 				"Porém até o momento poucas pessoas ficaram doentes. O que você aconselharia?                                 ",
-				"  ",
+				"",
 				"1 - Os lugares estão ocupados.                                                                               ",
-				"2 - Sim, está livre.                                                                                         ",
-				"  "
+				"2 - Sim, está livre.                                                                                         "
 		};
 		
-		imprimeTexto(enredo);
+		imprimeCaracter(enredo, 5);
+		System.out.println("");
 		System.out.print("Digite a sua resposta: ");
 		resposta = leitor.nextInt();
 
@@ -277,10 +299,82 @@ public class FiapLivroJogo {
 			System.out.println("Mário pensou dessa maneira por que em breve retornará pra casa. Sua preocupação é com sua familia.  ");
 			System.out.println("Ele não agiu de maneira grosseira, apenas se preveniu.");
 			break;
-		case 2:
+			
+		default:
 			System.out.println("Para Mario isso não seria nenhum problema visto que não há o que se preocupar. É só uma gripezinha. ");
 			break;
 		}
 		
+		
+	}
+	
+	
+	private static boolean cenaKlingdonNaMesa() {
+		int resposta;
+		
+		String[] enredo = {
+				"Na verdade os Klingdons perguntaram por ironia. Eles não tem modos e independentemente da",
+				"resposta eles iriam sentar do mesmo jeito. O problema é que eles falam gritando, gesticulam",
+				"muito, batem na mesa e salivam muito. E se deixar, pegam a sua comida e bebida ou oferece",
+				"a bebida popular altamente alcoólica para os humanos.",
+				"",
+				"E claro, encheram o copo do nosso amigo. O último que se recusou ou reclamou foi parar na",
+				"enfermaria ou como costumavam dizer, a sete palmos debaixo da terra. Recusá-la é altamente",
+				"ofensivo para eles. Vai entender, né?",
+				"",
+				"Na TV holográfica está passando notícias mais recentes dizendo que o planeta Klingdon a coisa",
+				"está cada vez mais séria. A suspeita é que um animal doente encontrado morto é o vetor zero.",
+				"Mas, o detalhe é que ele se alimenta de uma fruta que é a base dessa maldita bebida.",
+				"",
+				"Mario sabe disso por que no ano passado foi arrumar o computador responsável pela colheita e",
+				"produção da bebida. E lá não existe controle de qualidade. Frutos comidos e podres fazem parte",
+				"da composição da bebida!",
+				"",
+				"Um dos Klingdons propõe um brinde. Sabendo disso o que Mario deveria fazer?",
+				"",
+				"1 - Se recusa a beber. Se for parar na enfermaria pode voltar mais cedo pra casa.",
+				"2 - Bebe, achando que é a única alternativa em sair vivo.",
+				"3 - Dá uma de distraído, bate 'sem querer' o cotovelo no copo e o deixa cair."
+		};
+		
+		imprimeCaracter(enredo, 5);
+		System.out.println("");
+		System.out.print("Digite a sua resposta: ");
+		resposta = leitor.nextInt();
+		System.out.println("");
+		
+		switch(resposta) {
+		case 1:
+			System.out.println("Ao recusar a bebida, os dois Klingdons partem para cima do Mario. E algo surpreendente acontece.");
+			System.out.println("Segundo relatos ele parece que cresceu ao ser desafiado e ficou metalizado e forte com o adhamantium.");
+			System.out.println("No final os dois Klingdons é que foram na enfermaria! Depois disso todos passaram a respeitá-lo.");
+			System.out.println("");
+			
+			continuaSaga = true;
+			break;
+		case 2:
+			System.out.println("Como ele não recusou da primeira vez, todos os dias os Klingdons enche o caneco do nosso amigo.");
+			System.out.println("Porém, alguns dias depois, um dos Klingdons adoece e vira zumbi. O outro morre dias depois.");
+			System.out.println("O sistema de AI detectou todos que tiveram contato, deixando-os em quarentena.");
+			System.out.println("Mario morre semanas depois. A autópsia não foi clara se foi devido à doença ou à bebida que afetou o");
+			System.out.println("fígado e outros órgãos de tal maneira que nem um artificial resolveria.");
+			System.out.println("");
+			
+			statusAtual = StatusSaude.MORTO;
+			
+			return false;
+
+		default:
+			System.out.println("Um deles fica furioso com o descuido e desperdício e só não partiu para briga por que na semana passada.");
+			System.out.println("ele foi salvo pelo nosso engenheiro encanador. Esse é um dos poucos motivos para que eles não te matem.");
+			System.out.println("");
+			
+			continuaSaga = true;
+			break;
+			
+		}
+		
+		return continuaSaga;
+
 	}
 }
